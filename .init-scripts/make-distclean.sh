5834d4ded930a2f1b90883ce56a121af5b0198c3
Peter Brightwell <peter.brightwell@bbc.co.uk>
2021-01-16 22:46:04 +0000

    Remove _site on distclean

--- .init-scripts/make-distclean.sh
+++ .init-scripts/make-distclean.sh
@@ -3,4 +3,4 @@
 set -o errexit
 
 rm -rf source-repo branches tags index.md index-contents.md
-rm -rf node_modules/ yarn.lock package-lock.json .scripts/ .layouts/ _layouts/ assets/ raml2html-nmos-theme/
+rm -rf node_modules/ yarn.lock package-lock.json .scripts/ .layouts/ _layouts/ assets/ raml2html-nmos-theme/ _site/
